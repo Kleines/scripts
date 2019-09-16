7aeaa686a883d4a342d6f2bcef043e9d3a58be17
Kleines <notavalid@hotmail.com>
2019-09-16 11:06:42 -0400

    First Snapshot

---
+++ oraclebackup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#This script backs up the Oracle DB every night
+
+rm -f /backups/backup-oracle*
+
+if tar -zcvf /backups/backup-classfiles-`date +%F`.tar.gz /root/classfiles/*
+  then
+  echo "Oracle backup completed on `date`" >>/var/log/oraclebackuplog 
+
+  else
+  echo "Oracle backup failed on `date`" >>/var/log/oraclebackuplog
+  mail -s BACKUP FAILED jason.eckert@trios.com </var/log/oraclebackuplog
+fi
